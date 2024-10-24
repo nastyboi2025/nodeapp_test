8c93e14b4d464c7c497105491f31bac71c473560
nastyboi2025 <naser.ahmed@games4all.org.uk>
2024-10-25 00:29:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ var port = 4000
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.end('Hello World\n')
+  response.end('Hello Nas! This basic js site is working!!!\n')
 })
 
 server.listen(port)
