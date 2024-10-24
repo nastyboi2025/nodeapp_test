d517caf4c26dbc96fd1486ad5648ea619600017d
nastyboi2025 <naser.ahmed@games4all.org.uk>
2024-10-25 00:12:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var express = require('express');
+/*var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
@@ -12,4 +12,17 @@ app.get('/ready', function (req, res) {
     res.send('{ "response": " Great!, Its working!" }');
 });
 app.listen(process.env.PORT || 3000);
-module.exports = app;
+module.exports = app;*/
+
+var http = require('http')
+
+var port = 4000
+
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'})
+  response.end('Hello World\n')
+})
+
+server.listen(port)
+
+console.log('Server running at http://localhost:' + port)
