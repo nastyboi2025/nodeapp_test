d0d0a1951eeb852d2fcf0471afc4ccb8a29bcefc
nastyboi2025 <nas2003@hotmail.com>
2024-12-17 02:05:04 +0000

    More addings

---
+++ forargocdproject/README.md
@@ -0,0 +1,14 @@
+hello
+
+# nodeapp_test
+This is a test - lol
+
+Here we are using github actions to do ci/cd on a simple js website and then push to my dockerhub account
+
+docker pull nastyboi/testnodejsapp:main <br />
+docker run -d -p 4001:4000 image_id <br />
+
+Then view in browser localhost:4001
+
+A new update
+
---
+++ forargocdproject/index.js
@@ -0,0 +1,28 @@
+/*var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('{ "response": "Hello Nas" }');
+});
+
+app.get('/will', function (req, res) {
+    res.send('{ "response": "Hello World" }');
+});
+app.get('/ready', function (req, res) {
+    res.send('{ "response": " Great!, Its working!" }');
+});
+app.listen(process.env.PORT || 3000);
+module.exports = app;*/
+
+var http = require('http')
+
+var port = 4000
+
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'})
+  response.end('Hello Nas! This basic js site is working!!!\n')
+})
+
+server.listen(port)
+
+console.log('Server running at http://localhost:' + port)
